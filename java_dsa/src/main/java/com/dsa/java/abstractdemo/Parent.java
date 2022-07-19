de78d840385634974247b6da5bfe94b8b552d601
Vineet pandey <vineet.panday650@gmail.com>
2022-07-19 20:52:49 +0530

    msg

---
+++ java_dsa/src/main/java/com/dsa/java/abstractdemo/Parent.java
@@ -0,0 +1,6 @@
+package com.dsa.java.abstractdemo;
+
+public abstract class Parent {
+    abstract void career(String name);
+    abstract void partner(String name,int age);
+}
---
+++ java_dsa/src/main/java/com/dsa/java/abstractdemo/Son.java
@@ -0,0 +1,12 @@
+package com.dsa.java.abstractdemo;
+
+public class Son  extends Parent{
+    @Override
+    void career(String name) {
+    }
+
+    @Override
+    void partner(String name, int age) {
+        
+    }
+}
