826e715a291b8e77ffe3bb87f4ca09964309e384
Vineet pandey <vineet.panday650@gmail.com>
2022-09-07 12:28:03 +0530

    code done

--- java_dsa/src/main/java/com/dsa/java/Main_Java.java
+++ java_dsa/src/main/java/com/dsa/java/Main_Java.java
@@ -1,13 +1,22 @@
 package com.dsa.java;
 
 import javax.security.sasl.SaslClient;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main_Java {
     public static void main(String[] args) {
-        System.out.println("Hello World !");
+        Scanner input=new Scanner(System.in);
+        var inputValue= input.nextInt();
+        List<Integer> integerArray = Arrays.asList(98, 44, 23, 234, 898, 123, 3444, 5444, 9444);
 
-        Scanner scanner= new Scanner(System.in);
-        System.out.println(scanner.nextInt());
+       // integerArray.stream().filter(x->in)
+
+
+
+
     }
+
+
 }
