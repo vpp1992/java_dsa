99f07177ef9901a55972f353192f10ff5b7df3c0
vinee <vineet.panday650@gmail.com>
2022-09-13 21:57:10 +0530

    serach min max value

---
+++ java_dsa/src/main/java/com/dsa/java/linearserch/Array2DSearch.java
@@ -0,0 +1,30 @@
+package com.dsa.java.linearserch;
+
+import java.util.Arrays;
+
+public class Array2DSearch {
+    public static void main(String[] args) {
+        int [][] array2d={
+                {23,4,1},
+                {18,12,3,9},
+                {78,99,34,56},
+                {18,12}
+        };
+
+        int[] ints = search2D(array2d, 99);
+        System.out.println(Arrays.toString(ints));
+    }
+
+    static int[]search2D(int[][] arr,int target){
+        if (arr.length==0) return new int[]{-1};
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col]==target){
+                    return new int[]{row,col};
+                }
+            }
+        }
+        return new int[]{-1};
+    }
+
+}
