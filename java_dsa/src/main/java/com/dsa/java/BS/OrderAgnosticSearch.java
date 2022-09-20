e819072c80dffcba03d389e09e98c51fb3633064
vinee <vineet.panday650@gmail.com>
2022-09-20 22:02:19 +0530

    add order agnostic bs tree

--- java_dsa/src/main/java/com/dsa/java/BS/OrderAgnosticSearch.java
+++ java_dsa/src/main/java/com/dsa/java/BS/OrderAgnosticSearch.java
@@ -2,7 +2,37 @@ package com.dsa.java.BS;
 
 public class OrderAgnosticSearch {
     public static void main(String[] args) {
+       // int[] arr={-18,-12,1,3,4,5,6,7,8,9,12};
+        int[] arrDes={19,17,12,11,10,9,8,3,2,1};
+        System.out.println(orderAgnosticBs(arrDes,19));
+    }
 
+    static int orderAgnosticBs(int[] arr, int target) {
+        if (arr.length == 0) return -1;
+        int start = 0;
+        int end = arr.length - 1;
+        boolean asc = arr[start] < arr[end];
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target == arr[mid]) {
+                return mid;
+            }
+            if (asc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+
+        }
+        return -1;
     }
 
 }
